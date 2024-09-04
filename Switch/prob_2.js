02663d80e6cc17e695b9e3e8e85bd3d235380b3e
Joshitha-AS <asjoshitha2312@gmail.com>
2024-09-04 12:07:52 +0530

    Switch problem 2

---
+++ Switch/prob_2.js
@@ -0,0 +1,21 @@
+let colour = "yellow";
+switch (colour){
+    case "yellow":
+        console.log("sunshine");
+        break;
+    case "pink":
+        console.log("rose");
+        break;
+    case "black":
+        console.log("black");
+        break;
+    case "blue":
+        console.log("Today is Thursday");
+        break;
+    case "green":
+        console.log("Today is Friday");
+        break;
+    default:
+        console.log("Invalid day");
+        break;
+}
