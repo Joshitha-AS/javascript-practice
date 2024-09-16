0099f55d505affeda831e6f76af8aa6bc89b35aa
Joshitha-AS <asjoshitha2312@gmail.com>
2024-09-16 12:32:30 +0530

    Array with For Loop

---
+++ Homeworks/sum1.js
@@ -0,0 +1,18 @@
+let direction="north";
+switch (direction){
+    case "east":
+        console.log("robot in north");
+        break;
+    case "west":
+        console.log("robot in west direction");
+        break;
+    case "north":
+        console.log("robot in north");
+        break;
+    case "south":
+        console.log("robot in south direction");
+        break;  
+    default:
+        console.log("invalid");
+        break;  
+}
---
+++ Homeworks/sum2.js
@@ -0,0 +1,5 @@
+let num=["one","two","three","four","five"];
+n=num.length;
+for(i=n-1; i>=0; i--){
+    console.log(num[i]); 
+}
